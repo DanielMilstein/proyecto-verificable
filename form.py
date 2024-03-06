8c75cc8f8e01452856ab829ed58f647285c26192
DanielMilstein <dnmilstein@miuandes.cl>
2024-03-06 20:19:50 -0300

    Revert "Initial commit and basic form"
    This reverts commit 4ff9111c2039a26d4c131d152c244fe465aa8b34.

--- form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class MyForm(FlaskForm):
-	name = StringField('Name', validators=[DataRequired()])
-	last_name = StringField('Last Name', validators=[DataRequired()])
-	submit = SubmitField('Submit')
