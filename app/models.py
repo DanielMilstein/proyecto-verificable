836c2001454559c4abd8ab8fa055c34df6d4e24b
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2024-03-28 18:18:02 -0300

    fix(models.py):
    little foreign key syntaxis issue that was causing error when running schema.

--- app/models.py
+++ app/models.py
@@ -72,7 +72,7 @@ class implicados(db.Model):
 class propietario(db.Model):
 	multipropietario_id = db.Column(db.Integer, primary_key=True)
 	rut = db.Column(db.String(10), db.ForeignKey('persona.rut'))
-	rol = db.Column(db.String(20), db.ForeignKey('bienRaiz.rol'))
+	rol = db.Column(db.String(20), db.ForeignKey('bien_raiz.rol'))
 	porcentaje_derecho = db.Column(db.Float)
 
 	# bienes_raices = db.relationship('bienRaiz', back_populates='propietarios')
@@ -85,7 +85,7 @@ class propietario(db.Model):
 
 class multipropietario(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	rol = db.Column(db.String(20), db.ForeignKey('bienRaiz.rol') , primary_key=True)
+	rol = db.Column(db.String(20), db.ForeignKey('bien_raiz.rol') , primary_key=True)
 	fojas = db.Column(db.Integer)
 	fecha_inscripcion = db.Column(db.Date)
 	numero_inscripcion = db.Column(db.Integer)
