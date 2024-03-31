22db4fe37884db2279fe3c37ff4cd80488f0cd40
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2024-03-30 22:56:12 -0300

    feat(multipropietario): Find Comunas
    The AJAX query to find COMUNAS is properly implemented

--- app/controllers/pages.py
+++ app/controllers/pages.py
@@ -33,7 +33,7 @@ def form():
     form = MyForm()
 
     cnes = CNE.query.all()
-    comunas = comuna.query.all()
+    comunas = Comuna.query.all()
 
     if request.method == 'POST' and form.validate_on_submit():
 
@@ -51,16 +51,16 @@ def form():
 
         rol_search = f'{form.comuna.data}-{form.manzana.data}-{form.predio.data}'
         print(rol_search)
-        bien_raiz = bienRaiz.query.filter_by(rol=rol_search).first()
+        bien_raiz = BienRaiz.query.filter_by(rol=rol_search).first()
         if bien_raiz is None:
-            bien_raiz = bienRaiz(form.comuna.data, form.manzana.data, form.predio.data)
+            bien_raiz = BienRaiz(form.comuna.data, form.manzana.data, form.predio.data)
             db.session.add(bien_raiz)
         else:
             pass
 
 
 
-        new_form = formulario(
+        new_form = Formulario(
             cne=form.cne.data,
             rol=bien_raiz.rol,
             fojas=form.fojas.data,
@@ -81,15 +81,15 @@ def form():
 
 
         for adquiriente in adquirientesRut:
-            adquirientePersona = persona.query.filter_by(rut=adquiriente).first()
+            adquirientePersona = Persona.query.filter_by(rut=adquiriente).first()
             if adquirientePersona is None:
-                adquirientePersona = persona(adquiriente)
+                adquirientePersona = Persona(adquiriente)
                 db.session.add(adquirientePersona)
                 db.session.commit()
             else:
                 pass
 
-            adquirienteImplicado = implicados(
+            adquirienteImplicado = Implicados(
                 rut=adquiriente,
                 numero_atencion=new_form.numero_atencion,
                 porcentaje_derecho=adquirientesPorcentaje[adquirientesRut.index(adquiriente)],
@@ -99,15 +99,15 @@ def form():
             # new_form.implicados.append(adquirienteImplicado)
 
         for enajenante in enajenantesRut:
-            enajenantePersona = persona.query.filter_by(rut=enajenante).first()
+            enajenantePersona = Persona.query.filter_by(rut=enajenante).first()
             if enajenantePersona is None:
-                enajenantePersona = persona(enajenante)
+                enajenantePersona = Persona(enajenante)
                 db.session.add(enajenantePersona)
                 db.session.commit() 
             else:
                 pass
 
-            enajenanteImplicado = implicados(
+            enajenanteImplicado = Implicados(
                 rut=enajenante,
                 numero_atencion=new_form.numero_atencion,
                 porcentaje_derecho=enajenantesPorcentaje[enajenantesRut.index(enajenante)],
@@ -129,7 +129,7 @@ def form():
 @blueprint.route('/form_list', methods=['GET', 'POST'])
 def form_list():
     # List all the forms in the database
-    forms = formulario.query.all()
+    forms = Formulario.query.all()
 
 
 
@@ -141,41 +141,59 @@ def autocomplete():
     search = request.args.get('q', '')
     # Query your database for search suggestions based on the `search` term
     # This is a simplified example; adapt it to your actual data source
-    suggestions = [{'id': item.codigo_comuna, 'text': item.nombre_comuna} for item in comuna.query.filter(comuna.nombre_comuna.contains(search)).all()]
+    suggestions = [{'id': item.codigo_comuna, 'text': item.nombre_comuna} for item in Comuna.query.filter(Comuna.nombre_comuna.contains(search)).all()]
     return jsonify(results=suggestions)
 
 
 
 @blueprint.route('/buscar_multipropietarios', methods=['GET','POST'])
 def search_multipropietarios():
-    # Get inputs from the form
-    año = request.form.get('año')
-    comuna_name = request.form.get('comuna')
-    manzana = request.form.get('manzana')
-    predio = request.form.get('predio')
+    if request.method == 'POST':
+        # For POST requests, use request.form to access form data
+        año = request.form.get('año')
+        comuna_codigo = request.form.get('comuna')
+        manzana = request.form.get('manzana')
+        predio = request.form.get('predio')
+    elif request.method == 'GET':
+        # For GET requests, use request.args to access query parameters
+        año = request.args.get('año')
+        comuna_codigo = request.args.get('comuna')
+        manzana = request.args.get('manzana')
+        predio = request.args.get('predio')
+
+    print(año, comuna_codigo, manzana, predio)
 
     # Check if any of the inputs are None
-    if None in (año, comuna_name, manzana, predio):
+    if None in (año, comuna_codigo, manzana, predio):
         # If any input is None, render the template without performing the search
         return render_template('/multipropietario/multipropietario.html', propietarios_info=None)
-    
-    
-    # Find comuna based on comuna_name
-    comuna_obj = comuna.query.filter_by(nombre_comuna=comuna_name).first()
-    comuna_codigo = comuna_obj.codigo_comuna
 
+    # Continue with the rest of your code to process the form data
+
+    # Find comuna based on comuna_name
+    comuna_obj = Comuna.query.filter_by(codigo_comuna=comuna_codigo).first()
+    comuna_name = comuna_obj.nombre_comuna
+
+    # Find the bien_raiz ID based on the provided inputs
+    bien_raiz_id = BienRaiz.query.filter(
+        BienRaiz.comuna == int(comuna_codigo),
+        BienRaiz.manzana == int(manzana),
+        BienRaiz.predio == int(predio)
+    ).with_entities(BienRaiz.rol).scalar()
+    
+    print(bien_raiz_id)
     # Query multipropietario table based on the given inputs
-    query = multipropietario.query.filter(
-        multipropietario.ano_vigencia_inicial <= año,
-        (multipropietario.ano_vigencia_final >= año) | (multipropietario.ano_vigencia_final == None),
-        multipropietario.rol.like(f'%{comuna_codigo}/{manzana}/{predio}%')
+    query = Multipropietario.query.filter(
+        Multipropietario.ano_vigencia_inicial <= año,
+        (Multipropietario.ano_vigencia_final >= año) | (Multipropietario.ano_vigencia_final == None),
+        Multipropietario.rol.like(f'%{comuna_codigo}/{manzana}/{predio}%')
     )
     multipropietarios = query.all()
-
+    print(multipropietarios)
     # Retrieve extra information from propietario table using multipropietario_id's
     propietarios_info = []
     for mp in multipropietarios:
-        propietarios = propietario.query.filter_by(multipropietario_id=mp.id).all()
+        propietarios = Propietario.query.filter_by(multipropietario_id=mp.id).all()
         for prop in propietarios:
             propietarios_info.append({
                 'nombre_propietario': 'Random Name',  # You can generate random names here
