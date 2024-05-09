04a9844628b84901ba03da9a68b40a6ed90cb57d
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2024-05-09 01:22:01 -0400

    feat(algoritmo-compraventa): scenario 3 pretty much done

--- app/controllers/algoritmo_multipropietario/algoritmo_compraventa.py
+++ app/controllers/algoritmo_multipropietario/algoritmo_compraventa.py
@@ -31,6 +31,9 @@ class AlgoritmoCompraventa:
         previous_form = self.get_latest_form(all_forms)
         temp_storage = self.store_form_data(previous_form)
 
+        # THIS IS WRONG, NEED TO UPDATE THEIR PREVIOUS ANO_VIGENCIA_FINAL 
+        # => Method 'update_form' from multipropietarioTableHandler should do the trick.
+        # Current one should be None
         for entry in temp_storage:
             entry['ano_vigencia_final'] = form_data.get('fecha_inscripcion').year - 1
 
@@ -72,7 +75,7 @@ class AlgoritmoCompraventa:
     def store_form_data(self, previous_form):
         temp_storage = []
         if previous_form:
-            propietarios = self.multipropietario_handler.get_propietarios_by_multipropietario_id(previous_form.id)
+            propietarios = self.multipropietario_handler.propietario_handler.get_by_multipropietario_id(previous_form.id)
             for propietario in propietarios:
                 entry = {
                     'id': propietario.id,
@@ -125,6 +128,9 @@ class AlgoritmoCompraventa:
         previous_form = self.get_latest_form(all_forms)
         temp_storage = self.store_form_data(previous_form)
 
+        # THIS IS WRONG, NEED TO UPDATE THEIR PREVIOUS ANO_VIGENCIA_FINAL 
+        # => Method 'update_form' from multipropietarioTableHandler should do the trick.
+        # Current one should be None
         for entry in temp_storage:
             entry['ano_vigencia_final'] = form_data.get('fecha_inscripcion').year - 1
 
@@ -165,8 +171,59 @@ class AlgoritmoCompraventa:
         return 0 < sum(adquiriente.get('pctje_derecho', 0) for adquiriente in adquirientes) < 100 and len(adquirientes) == 1 and len(enajenantes) == 1
 
     def handle_scenario_3(self, form_data):
-        # Logic for scenario 3
-        pass
+        rol = form_data.get('rol')
+        all_forms = self.get_existing_forms(rol)
+        previous_form = self.get_latest_form(all_forms)
+        temp_storage = self.store_form_data(previous_form)
+
+        # THIS IS WRONG, NEED TO UPDATE THEIR PREVIOUS ANO_VIGENCIA_FINAL 
+        # => Method 'update_form' from multipropietarioTableHandler should do the trick.
+        # Current one should be None
+        for entry in temp_storage:
+            entry['ano_vigencia_final'] = form_data.get('fecha_inscripcion').year - 1
+
+        enajenantes_ruts = [enajenante.get('rut') for enajenante in form_data.get('enajenantes', [])]
+
+        for adquiriente in form_data.get('adquirientes', []):
+            entry = {
+                'id': None,
+                'rol': form_data.get("rol", None),
+                'fecha_inscripcion': form_data.get("fecha_inscripcion"),
+                'fojas': form_data.get("fojas", None),
+                'nro_inscripcion': form_data.get("nro_inscripcion"),
+                'rut': adquiriente.get("rut", None),
+                'ano_vigencia_final': None
+            }
+            temp_storage.append(entry)
+
+        for entry in temp_storage:
+            if entry['rut'] in enajenantes_ruts:
+                prev_porcentaje_derecho = entry['porcentaje_derecho']
+                for enajenante in form_data.get('enajenantes', []):
+                    if enajenante.get('rut') == entry['rut']:
+                        enajenante_porcentaje = enajenante.get('pctje_derecho')
+                        final_porcentaje_derecho_enajenante = prev_porcentaje_derecho - (enajenante_porcentaje * prev_porcentaje_derecho)
+                        entry['porcentaje_derecho'] = final_porcentaje_derecho_enajenante
+            else:
+                for adquiriente in form_data.get('adquirientes', []):
+                    if adquiriente.get('rut') == entry['rut']:
+                        adquiriente_porcentaje = adquiriente.get('pctje_derecho')
+                        final_porcentaje_derecho_adquiriente = adquiriente_porcentaje * entry['porcentaje_derecho']
+                        entry['porcentaje_derecho'] = final_porcentaje_derecho_adquiriente
+
+        temp_storage = [entry for entry in temp_storage if entry['porcentaje_derecho'] != 0]
+
+        for entry in temp_storage:
+            entry['ano_inscripcion'] = form_data.get('fecha_inscripcion').year
+            if entry['id'] is None:
+                entry['ano_vigencia_inicial'] = form_data.get('fecha_inscripcion').year
+
+        for entry in temp_storage:
+            if entry['id'] is None:
+                self.upload_multipropietario(entry)
+            else:
+                self.upload_propietario(entry)
+
 
     def handle_scenario_4(self, form_data):
         # Logic for scenario 4
